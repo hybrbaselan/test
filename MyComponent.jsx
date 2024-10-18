9e7c0ecb250fd8528d4c6d4acc5494251fb9fe14
Hybrbase_Lan <lan.tran@hybrbase.com>
2024-10-18 16:21:54 +0700

    update

--- MyComponent.jsx
+++ MyComponent.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import { Button } from 'antd';
 
 const MyComponent = ({ text }) => {
   return (
-    <motion.div
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-    >
+      <>
       <p>{text}</p>
       <Button type="primary">Click me</Button>
-    </motion.div>
+      </>
   );
 };
 
