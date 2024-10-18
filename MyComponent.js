f0c51c6e6e0e1a1d78db01a7c695294e145e4f3a
Hybrbase_Lan <lan.tran@hybrbase.com>
2024-10-18 16:14:07 +0700

    update

--- MyComponent.js
+++ MyComponent.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { Button } from 'antd';
 
 const MyComponent = ({ text }) => {
-  return React.createElement('div', null, text);
+  return (
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+    >
+      <p>{text}</p>
+      <Button type="primary">Click me</Button>
+    </motion.div>
+  );
 };
 
 export default MyComponent;
